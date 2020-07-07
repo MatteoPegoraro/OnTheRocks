c3361bb296a820e722076f46054879ed1388c3c4
Matteo Pegoraro <teoh27@hotmail.it>
2020-07-07 07:48:41 +0000

    adding route pages

--- app.js
+++ app.js
@@ -3,18 +3,23 @@ const axios = require("axios");
 
 const app = express();
 
+// redirect
+app.get('/', (req, res) => {
+  res.redirect('/home');
+});
 // homepage
 app.get("/home", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/public/homepage.html");
 });
 
 // cocktails page
 app.get('/cocktails',(req, res) => {
-  res.sendFile(__dirname, "/cocktails.html")
+  res.sendFile(__dirname, "/public/cocktail.html");
 })
 
+// pagina non trovata
 app.get("*", (req, res) => {
-  res.send("not found");
+  res.sendFile(__dirname, "/public/404.html");
 });
 
 app.listen(3000, () => console.log("listening -> port 3000"));
