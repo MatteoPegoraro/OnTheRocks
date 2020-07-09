33414aefa02dd818b6654769ac6f53a20e6c9604
Matteo Pegoraro <teoh27@hotmail.it>
2020-07-09 13:01:17 +0000

    adding ingredients random searc, fixing funcs.js

--- public/funcs.js
+++ public/funcs.js
@@ -1,14 +1,11 @@
-// filling the page
+// // filling the page
 function fillHTML(response) {
   let drink = response.data.drinks[0];
 
   // riempimento dati dell'html
   document.getElementById("image-cocktail").src = drink.strDrinkThumb;
   document.getElementById("name-cocktail").innerText = drink.strDrink;
-  document.getElementById("desc-cocktail").innerText =
-    drink.strInstructions;
-  document.getElementById("desc-cocktail").innerText =
-    drink.strInstructions;
+  document.getElementById("desc-cocktail").innerText = drink.strInstructions;
 
   // lista ingredienti gestita "pulendo" il json (grazie Matteo e Ale <3)
   const arrIngr = [];
@@ -50,45 +47,52 @@ function showRandomCocktail() {
 // inizializzione pagina attraverso paramentro Nome del cocktail (attraverso il form in homepage.html)
 function getCocktailFromSearch(name) {
   axios
-    .get(
-      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`
-    )
+    .get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
     .then((response) => fillHTML(response));
 }
 
 // inizializzione pagina attraverso paramentro Ingrediente del cocktail (attraverso il form in homepage.html)
 function getIngrsFromSearch(ingr) {
   axios
-    .get(
-      `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingr}`
-    )
-    .then((response) => fillHTML(response));
+    .get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingr}`)
+    .then((response) => {
+      let drink = response.data.drinks;
+      const maxRange = drink.length;
+      const randomNumber = Math.floor(Math.random() * maxRange);
+      const randomDrink = response.data.drinks[randomNumber];
+
+      let nameCocktail = randomDrink.strDrink;
+      axios
+        .get(
+          `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${nameCocktail}`
+        )
+        .then((response) => fillHTML(response));
+    });
 }
 
-
 let quotes = [];
-function fillQuotes(){
-quotes.push(
-  "By the time a bartender knows what drink a man will have before he orders, there is little else about him worth knowing."
-);
-quotes.push(
-  "24 hours in a day. 24 beers in a case. Coincidence? I think not."
-);
-quotes.push(
-  "You're not drunk if you can lie on the floor without holding on."
-);
-quotes.push(
-  "It takes only one drink to get me drunk.....the trouble is, I can't remember if it's the thirteenth or the fourteenth."
-);
-quotes.push(
-  "I like to have a martini, Two at the very most. After three I'm under the table, after four I'm under my host."
-);
-quotes.push(
-  "An intelligent man is sometimes forced to be drunk to spend time with his fools."
-);
-quotes.push(
-  "All right, brain. You don't like me and I don't like you, but let's just do this and I can get back to killing you with beer."
-);
+function fillQuotes() {
+  quotes.push(
+    "By the time a bartender knows what drink a man will have before he orders, there is little else about him worth knowing."
+  );
+  quotes.push(
+    "24 hours in a day. 24 beers in a case. Coincidence? I think not."
+  );
+  quotes.push(
+    "You're not drunk if you can lie on the floor without holding on."
+  );
+  quotes.push(
+    "It takes only one drink to get me drunk.....the trouble is, I can't remember if it's the thirteenth or the fourteenth."
+  );
+  quotes.push(
+    "I like to have a martini, Two at the very most. After three I'm under the table, after four I'm under my host."
+  );
+  quotes.push(
+    "An intelligent man is sometimes forced to be drunk to spend time with his fools."
+  );
+  quotes.push(
+    "All right, brain. You don't like me and I don't like you, but let's just do this and I can get back to killing you with beer."
+  );
 }
 fillQuotes();
 
@@ -99,4 +103,3 @@ function getRandomQuotes(quotes) {
   const randomQuotes = quotes[randomNumber];
   document.getElementById("quotes").innerText = randomQuotes;
 }
-
